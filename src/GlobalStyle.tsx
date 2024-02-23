867101a3e774444e656d2c0c00fd2392fe197f4f
D0uglasSantos <douglas.aquino@citechnology.com.br>
2024-02-23 11:08:47 -0300

    fix: height Global Container

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -11,11 +11,12 @@ export const Container = styled.section`
   padding: 2rem 0;
   width: 100%;
   transition: all ease 0.7s;
+  min-height: 100vh;
 
   @media screen and (max-width: 580) {
     align-items: left;
     justify-content: flex-start;
-    display: flex;
+    display: flex;''
     text-align: left;
   }
 `;
